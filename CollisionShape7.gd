182fb8b6729e8aa4c10c86ef0928bd16263d8267
Reneator <Reneator@googlemail..com>
2020-03-31 17:28:09 +0200

    Initial commit

---
+++ CollisionShape7.gd
@@ -0,0 +1,19 @@
+extends CollisionShape
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	rotate_object_local(Vector3(0,0,1), 1 *delta)
+	
+
+#func _physics_process(delta):
+#	rotate_object_local(Vector3(0,0,1), 1 *delta)
